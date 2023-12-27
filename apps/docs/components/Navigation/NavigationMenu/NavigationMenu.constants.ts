66d7449811fa40b79138e899f305a2cc06ee1a9c
Chris Gwilliams <encima@gmail.com>
2023-12-27 11:08:25 +0000

    add docs page on high swap

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -1434,6 +1434,10 @@ export const platform: NavMenuConstant = {
           name: 'High RAM Usage',
           url: '/guides/platform/exhaust-ram',
         },
+        {
+          name: 'High Swap Usage',
+          url: '/guides/platform/exhaust-swap',
+        },
       ],
     },
   ],
